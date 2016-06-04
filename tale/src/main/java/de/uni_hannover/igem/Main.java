5c108ea3c4587cf456c033960a7cdaca276397ab
dagon <Martin-Singer@gmx.net>
2016-06-04 16:39:11 +0200

    keine macht dem Lib-Ordner

--- tale/src/main/java/de/uni_hannover/igem/Main.java
+++ tale/src/main/java/de/uni_hannover/igem/Main.java
@@ -11,7 +11,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		List<Tale> talesequence = ExactScan.getTale("ACTACAAGKLSSENF");
-
 		try {
 			System.out.println(Misc.getCounterSequence("ACTACAAGPENIS"));
 		} catch (Exception e) {

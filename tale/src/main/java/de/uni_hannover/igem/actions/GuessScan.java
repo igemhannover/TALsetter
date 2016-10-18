b9fcc84eb8d7b5394c76f29399bfeaf46e7c1c85
hut <hut@lepus.uberspace.de>
2016-10-19 00:04:03 +0200

    limit length of TALEs in GuessScan

--- tale/src/main/java/de/uni_hannover/igem/actions/GuessScan.java
+++ tale/src/main/java/de/uni_hannover/igem/actions/GuessScan.java
@@ -3,6 +3,7 @@ package de.uni_hannover.igem.actions;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.uni_hannover.igem.util.Constants;
 import de.uni_hannover.igem.util.Misc;
 import de.uni_hannover.igem.util.ScanResult;
 import de.uni_hannover.igem.util.TALRater;
@@ -23,8 +24,15 @@ public class GuessScan {
 			for (int j = sequence.length(); j > i; j--) {
 				String possibleSequence = sequence.substring(i, j);
 				String taleSequence = Misc.getTaleSequence2DNASequence(possibleSequence);
-				double rating = TALRater.getRating(taleSequence);
+				double rating;
 				ScanResult result = new ScanResult(taleSequence, i);
+				if (((taleSequence.length() / 2) < Constants.getMinNucleaseDistance()) ||
+					((taleSequence.length() / 2) > Constants.getMaxNucleaseDistance())) {
+						rating = 0;
+				}
+				else {
+					rating = TALRater.getRating(taleSequence);
+				}
 				result.setRating(rating);
 				resultList.add(result);
 			}
